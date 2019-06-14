2d8252783632a14d47b773823a6bf2f0412f0dae
Naohisa Sakamoto <naohisa.sakamoto@people.kobe-u.ac.jp>
2019-06-15 01:17:27 +0900

    Modified.

--- App/PDEEstimation/main.cpp
+++ App/PDEEstimation/main.cpp
@@ -20,22 +20,25 @@
 #include "Label.h"
 #include "Regression.h"
 
+typedef kvs::StructuredVolumeObject Volume;
+
 int main( int argc, char** argv )
 {
     kvs::glut::Application app( argc, argv );
     kvs::glut::Screen screen( &app );
     screen.show();
 
-    kvs::StructuredVolumeObject* tornado = new kvs::TornadoVolumeData( kvs::Vec3u( 32, 32, 32 ) );
-    kvs::StructuredVolumeObject* S = local::Abs( tornado );
-    kvs::StructuredVolumeObject* U = local::U( tornado );
-    kvs::StructuredVolumeObject* V = local::V( tornado );
-    kvs::StructuredVolumeObject* W = local::W( tornado );
+    Volume* tornado = new kvs::TornadoVolumeData( kvs::Vec3u( 32, 32, 32 ) );
+    Volume* S = local::Abs( tornado );
+    Volume* U = local::U( tornado );
+    Volume* V = local::V( tornado );
+    Volume* W = local::W( tornado );
     delete tornado;
 
-    kvs::StructuredVolumeObject* dUdX = local::dX( U );
-    kvs::StructuredVolumeObject* dUdY = local::dY( U );
-    kvs::StructuredVolumeObject* d2UdX2 = local::dX( dUdX );
+    Volume* dU_dX = local::dX( U );
+    Volume* dU_dY = local::dY( U );
+    Volume* d2U_dX2 = local::dX( dU_dX );
+    Volume* d2U_dXdY = local::dY( dU_dX );
     delete U;
     delete V;
     delete W;
@@ -45,15 +48,17 @@ int main( int argc, char** argv )
     const size_t npoints = 100;
 
     const kvs::ValueArray<float> Y = local::Sampling( npoints, S, center, length );
-    const kvs::ValueArray<float> X0 = local::Sampling( npoints, dUdX, center, length );
-    const kvs::ValueArray<float> X1 = local::Sampling( npoints, dUdY, center, length );
-    const kvs::ValueArray<float> X2 = local::Sampling( npoints, d2UdX2, center, length );
+    const kvs::ValueArray<float> X0 = local::Sampling( npoints, dU_dX, center, length );
+    const kvs::ValueArray<float> X1 = local::Sampling( npoints, dU_dY, center, length );
+    const kvs::ValueArray<float> X2 = local::Sampling( npoints, d2U_dX2, center, length );
+    const kvs::ValueArray<float> X3 = local::Sampling( npoints, d2U_dXdY, center, length );
 
     local::Regression<float> regression;
     regression.setDependentVariable( Y );
     regression.addIndependentVariable( X0 );
     regression.addIndependentVariable( X1 );
     regression.addIndependentVariable( X2 );
+    regression.addIndependentVariable( X3 );
     regression.fit();
 
     screen.registerObject( local::Box( S, center, length ) );
